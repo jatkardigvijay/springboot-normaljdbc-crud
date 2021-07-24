230ee318f3fe02a4e6210cdf878203055fc0da92
jatkardigvijay <jatkardigvijay@gmail.com>
2021-07-24 19:13:28 +0530

    version change to 2.5.3 and queries change

--- src/main/java/com/jbd/utils/Queries.java
+++ src/main/java/com/jbd/utils/Queries.java
@@ -3,8 +3,8 @@ package com.jbd.utils;
 public class Queries {
 
 	public static final String GET_ALL_EMPLOYEES = "select * from employees";
-	public static final String DELETE_EMPLOYEE = "delete from employees where empId = ?";
+	public static final String DELETE_EMPLOYEE = "delete from employees where employee_Id = ?";
 	public static final String INSERT_EMPLOYEE = "insert into employees values (?,?,?)";
-	public static final String UPDATE_EMPLOYEE = "update employees set empName = ?, epAge = ? where empId = ?";
+	public static final String UPDATE_EMPLOYEE = "update employees set employee_Name = ?, employee_Age = ? where employee_Id = ?";
 	public static final String GET_EMPLOYEE = "select from employees where employee_Id = ?";
 }
