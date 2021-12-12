c4eaa3299973846a66fbfbc67215bf6cda46f261
jatkardigvijay <jatkardigvijay@gmail.com>
2021-12-12 16:41:29 +0530

    Swagger UI added

--- src/main/java/com/jbd/SpringbootCrudNormalJdbcApplication.java
+++ src/main/java/com/jbd/SpringbootCrudNormalJdbcApplication.java
@@ -3,7 +3,10 @@ package com.jbd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class SpringbootCrudNormalJdbcApplication {
 
 	public static void main(String[] args) {
