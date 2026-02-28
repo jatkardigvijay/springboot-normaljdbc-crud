6390677db4ea91bbb34d354bed07f70def23093a
Digvijay Jatkar <jatkardigvijay@gmail.com>
2026-03-01 04:33:05 +0530

    employees replaced by employee

--- src/main/java/com/jbd/utils/Queries.java
+++ src/main/java/com/jbd/utils/Queries.java
@@ -2,10 +2,10 @@ package com.jbd.utils;
 
 public class Queries {
 
-	public static final String GET_ALL_EMPLOYEES = "select * from employees";
-	public static final String DELETE_EMPLOYEE = "delete from employees where employeeId = ?";
-	public static final String INSERT_EMPLOYEE = "insert into employees values (?,?,?)";
-	public static final String UPDATE_EMPLOYEE = "update employees set employeeName = ?, employeeAge = ? where employeeId = ?";
-	public static final String GET_EMPLOYEE = "select * from employees where employeeId = ?";
+	public static final String GET_ALL_EMPLOYEES = "select * from employee";
+	public static final String DELETE_EMPLOYEE = "delete from employee where employeeId = ?";
+	public static final String INSERT_EMPLOYEE = "insert into employee values (?,?,?)";
+	public static final String UPDATE_EMPLOYEE = "update employee set employeeName = ?, employeeAge = ? where employeeId = ?";
+	public static final String GET_EMPLOYEE = "select * from employee where employeeId = ?";
 
 }
