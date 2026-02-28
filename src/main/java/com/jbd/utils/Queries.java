97e04a7b67d0f230f7f77d2bcd94abf189b131e5
Digvijay Jatkar <jatkardigvijay@gmail.com>
2026-03-01 04:57:37 +0530

    auto increment issue solved

--- src/main/java/com/jbd/utils/Queries.java
+++ src/main/java/com/jbd/utils/Queries.java
@@ -3,9 +3,9 @@ package com.jbd.utils;
 public class Queries {
 
 	public static final String GET_ALL_EMPLOYEES = "select * from employee";
-	public static final String DELETE_EMPLOYEE = "delete from employee where employeeId = ?";
-	public static final String INSERT_EMPLOYEE = "insert into employee values (?,?,?)";
-	public static final String UPDATE_EMPLOYEE = "update employee set employeeName = ?, employeeAge = ? where employeeId = ?";
 	public static final String GET_EMPLOYEE = "select * from employee where employeeId = ?";
-
+	public static final String DELETE_EMPLOYEE = "delete from employee where employeeId = ?";
+	public static final String INSERT_EMPLOYEE = "insert into employee (employeeName, employeeAge) values (?, ?)";
+	public static final String UPDATE_EMPLOYEE = "update employee set employeeName = ?, employeeAge = ? where employeeId = ?";
+	
 }
